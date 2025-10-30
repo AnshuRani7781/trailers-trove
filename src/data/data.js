36c7446d8d4fc3f35f9337ae017fb6d4c5ddbbba
Anshu Rani <ansh66605@gmail.com>
2025-10-31 00:31:35 +0530

    Add Oppenheimer movie details and thumbnails

--- src/data/data.js
+++ src/data/data.js
@@ -1,171 +1,192 @@
-
 export const movies = [
   {
     id: 1,
     title: "Creed III",
-    description: "When a childhood friend and former boxing prodigy, Damian (Jonathan Majors), resurfaces after serving a long sentence in prison, he is eager to prove that he deserves his shot in the ring. The face off between former friends is more than just a fight.",
-    thumbnail: '/thumbnails/creed3_p.jpeg',
-    backgroundImage: './thumbnails/creed_path.png',
-    trailer: '07gydPrJpP4',
+    description:
+      "When a childhood friend and former boxing prodigy, Damian (Jonathan Majors), resurfaces after serving a long sentence in prison, he is eager to prove that he deserves his shot in the ring. The face off between former friends is more than just a fight.",
+    thumbnail: "/thumbnails/creed3_p.jpeg",
+    backgroundImage: "./thumbnails/creed_path.png",
+    trailer: "07gydPrJpP4",
   },
   {
     id: 2,
     title: "John Wick: Chapter 4",
-    description: "John Wick is on the run for two reasons… he’s being hunted for a global $14 million dollar open contract on his life, and for breaking a central rule: taking a life on Continental Hotel grounds. The victim was a member of the High Table who ordered the open contract. John should have already been executed, except the Continental’s manager, Winston, has given him a one-hour grace period before he’s “Excommunicado” – membership revoked, banned from all services and cut off from other members. John uses the service industry to stay alive as he fights and kills his way out of New York City.",
-    thumbnail: '/thumbnails/john_wick.jpg',
-    backgroundImage: './thumbnails/jw_bg.jpeg',
-    trailer: 'yjRHZEUamCc'
+    description:
+      "John Wick is on the run for two reasons… he’s being hunted for a global $14 million dollar open contract on his life, and for breaking a central rule: taking a life on Continental Hotel grounds. The victim was a member of the High Table who ordered the open contract. John should have already been executed, except the Continental’s manager, Winston, has given him a one-hour grace period before he’s “Excommunicado” – membership revoked, banned from all services and cut off from other members. John uses the service industry to stay alive as he fights and kills his way out of New York City.",
+    thumbnail: "/thumbnails/john_wick.jpg",
+    backgroundImage: "./thumbnails/jw_bg.jpeg",
+    trailer: "yjRHZEUamCc",
   },
   {
     id: 3,
     title: "The Dark Knight",
-    description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-    thumbnail: '/thumbnails/tdn_p.png',
-    backgroundImage: './thumbnails/tdn_b.png',
-    trailer: 'LDG9bisJEaI'
+    description:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    thumbnail: "/thumbnails/tdn_p.png",
+    backgroundImage: "./thumbnails/tdn_b.png",
+    trailer: "LDG9bisJEaI",
   },
   {
     id: 4,
     title: "Spy Kids: Armageddon",
-    description: "When a game developer unleashes a powerful computer virus, the children of two secret agents must work together to save their parents — and the world.",
-    thumbnail: '/thumbnails/sk_p.png',
-    backgroundImage: './thumbnails/sk_b.png',
-    trailer: 'TuiRw0v3bAw'
+    description:
+      "When a game developer unleashes a powerful computer virus, the children of two secret agents must work together to save their parents — and the world.",
+    thumbnail: "/thumbnails/sk_p.png",
+    backgroundImage: "./thumbnails/sk_b.png",
+    trailer: "TuiRw0v3bAw",
   },
   {
     id: 5,
     title: "Captain MARVEL",
-    description: "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.",
-    thumbnail: '/thumbnails/cm_p.png',
-    backgroundImage: './thumbnails/cm_b.png',
-    trailer: 'OC3lhUTFdZY'
+    description:
+      "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.",
+    thumbnail: "/thumbnails/cm_p.png",
+    backgroundImage: "./thumbnails/cm_b.png",
+    trailer: "OC3lhUTFdZY",
   },
   {
     id: 6,
     title: "The Lord of the Rings",
-    description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
-    thumbnail: '/thumbnails/lor_p.png',
-    backgroundImage: './thumbnails/lor_b.png',
-    trailer: 'x8UAUAuKNcU'
+    description:
+      "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+    thumbnail: "/thumbnails/lor_p.png",
+    backgroundImage: "./thumbnails/lor_b.png",
+    trailer: "x8UAUAuKNcU",
   },
-  
+
   {
     id: 7,
     title: "Aquaman and the Lost Kingdom",
-    description: "Arthur must enlist the help of his half-brother Orm in order to protect Atlantis against Black Manta, who has unleashed a devastating weapon in his obsessive quest to avenge his father's death.",
-    thumbnail: '/thumbnails/aquaman_p.png',
-    backgroundImage: './thumbnails/aquaman.jpeg',
-    trailer: 'FV3bqvOHRQo'
+    description:
+      "Arthur must enlist the help of his half-brother Orm in order to protect Atlantis against Black Manta, who has unleashed a devastating weapon in his obsessive quest to avenge his father's death.",
+    thumbnail: "/thumbnails/aquaman_p.png",
+    backgroundImage: "./thumbnails/aquaman.jpeg",
+    trailer: "FV3bqvOHRQo",
   },
   {
     id: 8,
     title: "Tiger 3",
-    description: "Following the events of Tiger Zinda Hai, War and Pathaan, Tiger's informed about a new military group that is planning something beyond all imagination. A deeply motivated Aatish Rehman, who has a history of his own with Tiger, takes control of the new military group.",
-    thumbnail: '/thumbnails/tiger3_path.png',
-    backgroundImage: './thumbnails/3tiger.png',
-    trailer: 'vEjTUDjjU6A'
+    description:
+      "Following the events of Tiger Zinda Hai, War and Pathaan, Tiger's informed about a new military group that is planning something beyond all imagination. A deeply motivated Aatish Rehman, who has a history of his own with Tiger, takes control of the new military group.",
+    thumbnail: "/thumbnails/tiger3_path.png",
+    backgroundImage: "./thumbnails/3tiger.png",
+    trailer: "vEjTUDjjU6A",
   },
   {
-
     id: 9,
     title: "Percy Jackson and the Olympians",
-    description: "A teenager discovers he's the descendant of a Greek god and sets out on an adventure to settle an on-going battle between the gods.",
-    thumbnail: '/thumbnails/pj_p.png',
-    backgroundImage: './thumbnails/pj_b1.png',
-    trailer: 'iuBkjqGWRbk'
+    description:
+      "A teenager discovers he's the descendant of a Greek god and sets out on an adventure to settle an on-going battle between the gods.",
+    thumbnail: "/thumbnails/pj_p.png",
+    backgroundImage: "./thumbnails/pj_b1.png",
+    trailer: "iuBkjqGWRbk",
   },
   {
-
     id: 10,
     title: "Monarch: Legacy of Monsters",
-    description: "Set after the battle between Godzilla and the Titans, revealing that monsters are real, follows one family's journey to uncover its buried secrets and a legacy linking them to Monarch.",
-    thumbnail: '/thumbnails/mlm_p.png',
-    backgroundImage: './thumbnails/mlm_bg.jpeg',
-    trailer: 'ec27RCzb6qI'
+    description:
+      "Set after the battle between Godzilla and the Titans, revealing that monsters are real, follows one family's journey to uncover its buried secrets and a legacy linking them to Monarch.",
+    thumbnail: "/thumbnails/mlm_p.png",
+    backgroundImage: "./thumbnails/mlm_bg.jpeg",
+    trailer: "ec27RCzb6qI",
   },
   {
-
     id: 11,
     title: "The Marvels",
-    description: "Marvel's trailer promises a cosmic collision of heroes, villains, and jaw-dropping spectacle that will leave you counting down the seconds until release day",
-    thumbnail: '/thumbnails/tm_p.png',
-    backgroundImage: './thumbnails/tm_b.png',
-    trailer: 'wg5liXvHl_Q'
+    description:
+      "Marvel's trailer promises a cosmic collision of heroes, villains, and jaw-dropping spectacle that will leave you counting down the seconds until release day",
+    thumbnail: "/thumbnails/tm_p.png",
+    backgroundImage: "./thumbnails/tm_b.png",
+    trailer: "wg5liXvHl_Q",
   },
   {
-
     id: 12,
     title: "Loki 2",
-    description: "Loki grapples with the multiverse's chaos in Season 2, navigating unpredictable timelines and facing new challenges.",
-    thumbnail: '/thumbnails/loki2_p.png',
-    backgroundImage: './thumbnails/loki2_b.png',
-    trailer: 'vwSKatRviQo'
+    description:
+      "Loki grapples with the multiverse's chaos in Season 2, navigating unpredictable timelines and facing new challenges.",
+    thumbnail: "/thumbnails/loki2_p.png",
+    backgroundImage: "./thumbnails/loki2_b.png",
+    trailer: "vwSKatRviQo",
   },
   {
-
     id: 13,
     title: "Deadpool 3",
-    description: "Deadpool 3 unleashes the Merc with a Mouth on a time-bending escapade, blending chaos, humor, and a dash of reluctant heroism as Wade Wilson faces his wildest ride yet.",
-    thumbnail: '/thumbnails/d3_p.png',
-    backgroundImage: './thumbnails/d3_b.png',
-    trailer: 'gfMRgap9jWk'
+    description:
+      "Deadpool 3 unleashes the Merc with a Mouth on a time-bending escapade, blending chaos, humor, and a dash of reluctant heroism as Wade Wilson faces his wildest ride yet.",
+    thumbnail: "/thumbnails/d3_p.png",
+    backgroundImage: "./thumbnails/d3_b.png",
+    trailer: "gfMRgap9jWk",
   },
   {
-
     id: 14,
     title: "Captain America: Brave New World",
-    description: "In Captain America: Brave New World, Steve Rogers grapples with a changed world as he navigates political intrigue and faces unexpected adversaries, all while staying true to his iconic ideals of justice and freedom.",
-    thumbnail: '/thumbnails/canwo_p.png',
-    backgroundImage: './thumbnails/canwo_b.png',
-    trailer: 'lCT49rPqU68'
+    description:
+      "In Captain America: Brave New World, Steve Rogers grapples with a changed world as he navigates political intrigue and faces unexpected adversaries, all while staying true to his iconic ideals of justice and freedom.",
+    thumbnail: "/thumbnails/canwo_p.png",
+    backgroundImage: "./thumbnails/canwo_b.png",
+    trailer: "lCT49rPqU68",
   },
   {
     id: 15,
     title: "Antman and the Wasp: Quantumania",
-    description: "The movie explores the Quantum Realm in detail and reveals Hope's history with Kang, showcasing her complicated past in this world",
-    thumbnail: '/thumbnails/am.jpeg',
-    backgroundImage: './thumbnails/antman_bg.png',
-    trailer: 'ZlNFpri-Y40'
+    description:
+      "The movie explores the Quantum Realm in detail and reveals Hope's history with Kang, showcasing her complicated past in this world",
+    thumbnail: "/thumbnails/am.jpeg",
+    backgroundImage: "./thumbnails/antman_bg.png",
+    trailer: "ZlNFpri-Y40",
   },
-   {
+  {
     id: 16,
     title: "Woman of the Hour",
-    description: "A tight true-crime thriller set against the backdrop of a cheesy television game show. This gripping narrative highlights just how strange truth can be, powered by stellar performances and a retro feel that reminds us of the value of modern DNA evidence.",
+    description:
+      "A tight true-crime thriller set against the backdrop of a cheesy television game show. This gripping narrative highlights just how strange truth can be, powered by stellar performances and a retro feel that reminds us of the value of modern DNA evidence.",
     thumbnail: "/thumbnails/woman_of_hour.jpg",
     backgroundImage: "./thumbnails/womanofthehour_bg.png",
-    trailer: "ODUdpwddTQk"
+    trailer: "ODUdpwddTQk",
   },
-   {
+  {
     id: 17,
     title: "Deadpool & Wolverine",
-    description: "Deadpool is offered a place in the Marvel Cinematic Universe by the Time Variance Authority, but instead recruits a variant of Wolverine to save his universe from extinction.",
+    description:
+      "Deadpool is offered a place in the Marvel Cinematic Universe by the Time Variance Authority, but instead recruits a variant of Wolverine to save his universe from extinction.",
     thumbnail: "/thumbnails/deadpool_wolverine.png",
     backgroundImage: "./thumbnails/deadpool_wolverine_bg.png",
-    trailer: "73_1biulkYk"
+    trailer: "73_1biulkYk",
   },
   {
     id: 18,
     title: "Kung Fu Panda 4",
-    description: "This spring, Jack Black returns as Po, the world’s most unlikely kung fu master, in a hilarious, action-packed new chapter. After three epic adventures, Po, the Dragon Warrior, is called to step up as the Spiritual Leader of the Valley of Peace. But first, he must find and train a new Dragon Warrior to take his place.",
+    description:
+      "This spring, Jack Black returns as Po, the world’s most unlikely kung fu master, in a hilarious, action-packed new chapter. After three epic adventures, Po, the Dragon Warrior, is called to step up as the Spiritual Leader of the Valley of Peace. But first, he must find and train a new Dragon Warrior to take his place.",
     thumbnail: "/thumbnails/kungfupanda4.png",
     backgroundImage: "./thumbnails/kp4_b.jpg",
-    trailer: "_inKs4eeHiI"
+    trailer: "_inKs4eeHiI",
   },
- {
+  {
     id: 19,
     title: "Panda Plan",
-    description: "When Jackie Chan is invited to adopt a beloved zoo panda named Hu Hu, a notorious crime syndicate sets a bounty on the bear. Jackie teams up with his agent and Hu Hu's devoted caretaker, embarking on an unforgettable adventure to outsmart and outfight the criminals at every turn.",
+    description:
+      "When Jackie Chan is invited to adopt a beloved zoo panda named Hu Hu, a notorious crime syndicate sets a bounty on the bear. Jackie teams up with his agent and Hu Hu's devoted caretaker, embarking on an unforgettable adventure to outsmart and outfight the criminals at every turn.",
     thumbnail: "/thumbnails/pandaplan.png",
     backgroundImage: "./thumbnails/pplan.png",
-    trailer: "ksls6lIiSPg"
+    trailer: "ksls6lIiSPg",
   },
   {
     id: 20,
     title: "Napoleon",
-    description: "An epic historical drama that chronicles the rise and fall of French military leader Napoleon Bonaparte, following his origins and his swift, ruthless climb to emperor through the lens of his addictive and volatile relationship with Josephine.",
-    thumbnail: '/thumbnails/napoleon_p.png',
-    backgroundImage: './thumbnails/napoleon_b.png',
-    trailer: 'OAZWXUkrjPc'
-  }
-  
+    description:
+      "An epic historical drama that chronicles the rise and fall of French military leader Napoleon Bonaparte, following his origins and his swift, ruthless climb to emperor through the lens of his addictive and volatile relationship with Josephine.",
+    thumbnail: "/thumbnails/napoleon_p.png",
+    backgroundImage: "./thumbnails/napoleon_b.png",
+    trailer: "OAZWXUkrjPc",
+  },
+  {
+    id: 21,
+    title: "Oppenheimer",
+    description:
+      "A biographical drama chronicling the life of J. Robert Oppenheimer and his role leading the Manhattan Project and the moral consequences that followed.",
+    thumbnail: "/thumbnails/oppenheimer_p.png",
+    backgroundImage: "./thumbnails/oppenheimer_b.png",
+    trailer: "bK6ldnjE3Y0",
+  },
 ];
